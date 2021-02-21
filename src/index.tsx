0846066424afdd2792fa07686f5173e32649bbbb
Boss Udomsuktham <bossiccs@Bosss-MacBook-Pro.local>
2021-02-21 19:28:27 +0700

    added cat logo

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 
 import {
-  HomeScreen,
+  // HomeScreen,
   LoginScreen,
   Dashboard,
 } from './screens';
