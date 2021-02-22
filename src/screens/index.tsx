5eec823ad3e33e07503cc78b800e42b231ed968e
MeYu-dot <77969821+MeYu-dot@users.noreply.github.com>
2021-02-22 17:01:11 +0700

    Update index.tsx

--- src/screens/index.tsx
+++ src/screens/index.tsx
@@ -2,3 +2,4 @@ export { default as LoginScreen } from './LoginScreen';
 export { default as Dashboard } from './Dashboard';
 export { default as RegisterScreen } from './RegisterScreen';
 export { default as AuthLoadingScreen } from './AuthLoadingScreen';
+export { default as ForgotPasswordScreen } from './ForgotPasswordScreen';
